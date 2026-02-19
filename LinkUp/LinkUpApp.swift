905be28fe4784eb3b5d3469399e8ef3490c8b12a
KTApps <mahajakelvin@gmail.com>
2026-02-19 10:54:44 +0000

    Initial Commit

---
+++ LinkUp/LinkUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkUpApp.swift
+//  LinkUp
+//
+//  Created by Kelvin Mahaja on 19/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct LinkUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LinkUpUITests/LinkUpUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LinkUpUITestsLaunchTests.swift
+//  LinkUpUITests
+//
+//  Created by Kelvin Mahaja on 19/02/2026.
+//
+
+import XCTest
+
+final class LinkUpUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
